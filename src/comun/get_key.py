77dbc97ad62dd3404bb513e174f607aedc22a17a
Andy Garcia <andreayg@ucm.es>
2026-03-15 01:01:30 +0100

    prueba en archivo de analisis

--- src/comun/get_key.py
+++ src/comun/get_key.py
@@ -3,6 +3,6 @@
 import json
 
 def get_key():
-    with open("Private/Claves.json", "r", encoding="utf-8") as archivo:
+    with open("../Private/Claves.json", "r", encoding="utf-8") as archivo:
         keys = json.load(archivo)
     return keys
