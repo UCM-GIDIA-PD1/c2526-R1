6d2add6d844da48f2b9045f5316cb214dcde8178
Luis Lopez Rodriguez <luilop09@ucm.es>
2026-03-18 15:40:32 +0100

    Añadido parametros

--- src/training_utils.py
+++ src/training_utils.py
@@ -3,18 +3,18 @@ from sklearn.model_selection import KFold
 from sklearn.pipeline import Pipeline
 from sklearn.decomposition import TruncatedSVD 
 from sklearn.metrics import accuracy_score, classification_report
-from preprocess_utils import build_preprocess, download_and_divide
+from preprocess_utils import build_preprocess, unzip_params
 from filter_and_divide_data import get_data_models_train_test
 import numpy as np
 import pandas as pd
 
-def run_cross_validation(X_train, y_train, preprocess_type, columns, parameter_name, parameter_vals, modelo, n_splits=5):
+def run_cross_validation(X_train, y_train, preprocess_type, columns, params, modelo, n_splits=5):
     best_param = None
     best_acc = 0
     
     kf = KFold(n_splits=n_splits, shuffle=True, random_state=42) #n splits 5
-
-    scores_dict = {k: [] for k in parameter_vals}
+    params_ = unzip_params(params=params)
+    scores_dict = [] #{k.keys()[0]: [] for k in params_} #Revisad
     for train_idx, val_idx in tqdm(kf.split(X_train)):
         X_tr, X_val = X_train.iloc[train_idx], X_train.iloc[val_idx]
         y_tr, y_val = y_train.iloc[train_idx], y_train.iloc[val_idx]
@@ -27,24 +27,25 @@ def run_cross_validation(X_train, y_train, preprocess_type, columns, parameter_n
         X_tr_trans = pipe.fit_transform(X_tr, y_tr)
         X_val_trans = pipe.transform(X_val)
 
-        for param_val in parameter_vals: #un array
-            model = modelo(param_val)#parameter_name = param_val) #KNeighborsClassifier(n_neighbors=k, metric="cosine")
+        for param_val in params_: #un array
+            model = modelo(**param_val)#parameter_name = param_val) #KNeighborsClassifier(n_neighbors=k, metric="cosine")
             model.fit(X_tr_trans, y_tr)
 
             preds = model.predict(X_val_trans)
             acc = accuracy_score(y_val, preds)
 
-            scores_dict[param_val].append(acc)
+            scores_dict.append((param_val, acc))
+            #scores_dict[param_val].append(acc)
 
     mean_acc_scores = {k: np.mean(v) for k, v in scores_dict.items()}
     for k in mean_acc_scores.keys():
-        print(f"{parameter_name}={k} -> CV accuracy: {mean_acc_scores[k]:.4f}")
+        print(f"{param_val}={k} -> CV accuracy: {mean_acc_scores[k]:.4f}")
 
         if mean_acc_scores[k] > best_acc:
             best_acc = mean_acc_scores[k]
             best_param = k
 
-    print(f"\nMejor {parameter_name} encontrado: {best_param}")
+    print(f"\nMejor {param_val} encontrado: {best_param}")
     print(f"CrossVal accuracy: {best_acc:.4f}")
 
     return best_acc, best_param
