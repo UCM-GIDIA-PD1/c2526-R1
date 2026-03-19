1464484095e645917ec9519e0e5d061f4ae8d2f7
Maria <marmar98@ucm.es>
2026-03-19 19:36:17 +0100

    Prueba con filtrado y best params

--- src/modelo_randomForest_generos.py
+++ src/modelo_randomForest_generos.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
     
     preprocess_type = "Word2Vec"
 
-    params = { "criterion": ["entropy"], "n_estimators": [10, 20, 30], "max_depth":[10, 15, 20], "max_features":[0.5]}
+    params = { "criterion": ["entropy"], "n_estimators": [40], "max_depth":[15], "max_features":[0.5]}
 
     n_fold = 5
 
-    filtrado = False
+    filtrado = True
 
     entrenamiento(RandomForestClassifier, to_predict, preprocess_type, columns, 
                   params, n_fold, filtrado)
