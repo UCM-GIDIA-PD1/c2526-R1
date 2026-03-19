d9c04ec12ff1e075681b7cffd4c46814a796ebfa
Maria <marmar98@ucm.es>
2026-03-19 09:42:22 +0100

    Cambios en rf-generos (prueba plantilla)

--- src/modelo_randomForest_generos.py
+++ src/modelo_randomForest_generos.py
@@ -1,4 +1,4 @@
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 from training_utils import entrenamiento
 
 if __name__ == '__main__':
@@ -7,11 +7,11 @@ if __name__ == '__main__':
     
     preprocess_type = "Word2Vec"
 
-    params = { "criterion": "entropy", "n_estimators": [10, 20, 30], "max_depth":[10, 15, 20], "max_features":	[0.5]}
+    params = { "criterion": ["entropy"], "n_estimators": [10, 20, 30], "max_depth":[10, 15, 20], "max_features":[0.5]}
 
     n_fold = 5
 
     filtrado = False
 
     entrenamiento(RandomForestClassifier, to_predict, preprocess_type, columns, 
-                  params, n_fold, filtrado= true)
+                  params, n_fold, filtrado)
