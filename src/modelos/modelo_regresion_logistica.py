317f47f87d5b2021add69c4cf62da996bac4c0b0
Andrea Garcia <andreayg@ucm.es>
2026-03-25 16:29:57 +0100

    modelo regLog generos

--- src/modelos/modelo_regresion_logistica.py
+++ src/modelos/modelo_regresion_logistica.py
@@ -1,26 +1,30 @@
 from sklearn.linear_model import LogisticRegression
 from training_utils import entrenamiento
+from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score
+
 
 if __name__ == '__main__':
     to_predict = "Generos"
-    columns = ["Titulo", "Descripcion", "Tags", "Subtitulos", "Rango_edad", "Duracion"]
+    columns = ["Titulo", "Descripcion", "Tags", "Subtitulos", "Made for kids", "Duracion", "Subgeneros", "Titulo_canal"]
     
+    max_features = 5000
+    ngram = (1,2)
+    svd = 150
+
+    project = "Regresion logística géneros"
+    name = "V0.0.1"
     preprocess_type = "Word2Vec"
 
-    # param_name = "n_neighbours"
-    # param_vals = range(3,5)
-    # metric = "cosine"
-
     params = {
-        "C": [0.1, 1, 10],
-        "solver": ["lbfgs"],
-        "max_iter": [1000],
-        "multi_class": "multinomial"
+        "C": [0.01, 0.1, 1, 10],
+        "solver": ["saga", "lbfgs"],
+        "max_iter": [1000, 2000, 3000]
     }
-
-    n_fold = 2
-
+    
+    score = "Precision"
+    average = "macro"
+    n_fold = 5
     filtrado = False
 
-    entrenamiento(LogisticRegression, to_predict, preprocess_type, columns, 
-                  params, n_fold, filtrado)
+    entrenamiento(project, name, LogisticRegression, to_predict, max_features, ngram, svd, preprocess_type, columns, 
+                params, score, average, n_fold, filtrado)
