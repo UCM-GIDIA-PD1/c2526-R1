7ae5f83c41cef08d4b22fe20f891542a66423d79
Anguie Ruiz <anguieru@ucm.es>
2026-03-25 13:14:51 +0100

    Creación Modelo inicial NB para Generos

---
+++ src/modelo_naive_bayes_generos.py
@@ -0,0 +1,19 @@
+from training_utils_naive_bayes import entrenamiento_nb
+
+if __name__ == "__main__":
+    to_predict = "Generos"
+    columns = ["Titulo", "Descripcion", "Tags", "Subtitulos"]
+
+    project = "Modelo Naive Bayes generos"
+    name = "V0"
+    preprocess_type = "TF-IDF"
+    params = {"alpha": [0.001, 0.005, 0.01, 0.02, 0.05, 0.1, 0.5, 1.0]}
+    n_fold = 5
+    score = "Recall"
+    average = "macro"
+    filtrado = True
+
+    entrenamiento_nb(
+        project, name, to_predict, preprocess_type, columns,
+        params, score, average, n_fold, filtrado
+    )
