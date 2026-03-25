f71b58bf1251d6630ebb1ff19c51b5df25547636
Maria <marmar98@ucm.es>
2026-03-25 12:37:41 +0100

    Mejor modelo encontrado RF generos - score: 0.66

--- src/modelo_randomForest_generos.py
+++ src/modelo_randomForest_generos.py
@@ -5,12 +5,12 @@ if __name__ == '__main__':
     to_predict = "Generos"
     columns = ["Titulo", "Descripcion", "Tags", "Subtitulos", "Rango_edad", "Duracion"]
     
-    project = "Prueba rf generos"
+    project = "Modelo rf generos"
     name = "V0"
 
     preprocess_type = "TF-IDF"
 
-    params = { "criterion": ["entropy"], "n_estimators": [40], "max_depth":[15], "max_features":[None]}
+    params = { "criterion": ["entropy"], "n_estimators": [40], "max_depth":[15], "max_features":[0.8]}
 
     n_fold = 5
 
