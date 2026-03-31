e7a19b65071223f71bee01fb49df64c153d63c11
Anguie Ruiz <anguieru@ucm.es>
2026-03-31 15:16:01 +0200

    Ajustando training utils para kids

---
+++ src/modelo_optuna_nb_kids.py
@@ -0,0 +1,22 @@
+from filter_and_divide_data import get_data_models_train_test
+from training_utils_optuna import run_optuna_nb
+
+if __name__ == "__main__":
+    to_predict = "Made for kids"
+    columns = ["Titulo", "Descripcion", "Tags", "Subtitulos"]
+    
+    X_train, X_test, y_train, y_test = get_data_models_train_test(
+        filtrado=True, to_predict=to_predict
+    )
+
+    # Ejecutar Optuna
+    best_alpha = run_optuna_nb(
+        project_="Modelo Optuna Naive Bayes Made for kids",
+        name="V0",
+        X_train=X_train,
+        y_train=y_train,
+        preprocess_type="TF-IDF",
+        columns=columns,
+        score_name="Precision",
+        n_trials=20
+    )
--- src/training_utils_optuna.py
+++ src/training_utils_optuna.py
@@ -24,16 +24,21 @@ def objective(trial, X_train, y_train, preprocess_type, columns, score_name):
         ("model", MultinomialNB(alpha=alpha_suggested))
     ])
 
-    metric = "f1_weighted" if score_name == "F1" else "precision_weighted"
+    if score_name.upper() == "F1":
+        metric = "f1_weighted"
+    elif score_name.upper() == "PRECISION":
+        metric = "precision_weighted"
+    else:
+        metric = "accuracy" #Por defecto
     
-    scores = cross_val_score(pipe, X_train, y_train, cv=5, scoring=metric, n_jobs=1) #Usa un solo núcleo para evitar problemas de paralelización con Optuna
+    scores = cross_val_score(pipe, X_train, y_train, cv=5, scoring=metric, n_jobs=1) #Evita problemas de paralelización con Optuna
     current_score = scores.mean()
 
     return current_score
 
 def run_optuna_nb(project_, name, X_train, y_train, preprocess_type, columns, score_name, n_trials=20):
 
-    wandb.init(project=project_, name=f"{name}_optuna")
+    wandb.init(project=project_, name=f"{name}_optuna_{score_name}")
 
     study = optuna.create_study(direction="maximize")
     
@@ -44,9 +49,11 @@ def run_optuna_nb(project_, name, X_train, y_train, preprocess_type, columns, sc
 
     wandb.log({
         "best_alpha_optuna": study.best_params["alpha"],
-        "best_cv_score_optuna": study.best_value
+        "best_cv_score_optuna": study.best_value,
+        "metric_used": score_name
     })
 
+    print(f"\n--- RESULTADOS OPTUNA ({score_name}) ---")
     print(f"\n[OPTUNA] Mejor Alpha encontrado: {study.best_params['alpha']}")
     print(f"[OPTUNA] Mejor Score de CV: {study.best_value:.4f}")
 
