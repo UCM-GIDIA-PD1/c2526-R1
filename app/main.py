1602d3acd562670f7adbcd80ac4ad31d83eaa764
Maria <marmar98@ucm.es>
2026-04-17 13:24:45 +0200

    Movido main de app la carpeta de proyecto y main de src a app. (tenemos que conectarnos con minio)

--- app/main.py
+++ app/main.py
@@ -1,58 +1,115 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+# Bibliotecas
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Request, Form
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+from pydantic import BaseModel, HttpUrl         #HttpUrl - adicional
+from joblib import load
+# Adicionales
+from extraccion.get_video_info_api import get_info
+import re
+import os
 
-from pydantic import BaseModel
-#Hay que definir el BaseModel
-app = FastAPI()
+# Ciclo de vida
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.model_genre = load('pd1/grupo1/models/genres/genres_definitive.joblib')
+    app.state.encoder_genre = load('pd1/grupo1/models/genres/encoder.joblib') # encoder
+    app.state.model_kids = load('pd1/grupo1/models/kids/kids_definitive.joblib')
+    yield
 
-templates = Jinja2Templates(directory="templates")
-class VideoRequest(BaseModel):
-    url: str
+# Web
+app = FastAPI(lifespan=lifespan)
 
-class VideoResponse(BaseModel):
-    safe: bool
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-@app.post("/video/check", response_model=VideoResponse)
-def check_video(data: VideoRequest):
-    print(data.url)
-    #Simulación para probar web
-    if "kids" in data.url.lower():
-        return VideoResponse(safe=True)
-    else:
-        return VideoResponse(safe=False)
-    
-@app.get("/video", response_class=HTMLResponse)
-def video_page(request: Request):
-    return templates.TemplateResponse(request,
-                                      name = "video_check.html",
-                                      context = {"request": request})
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
-class GenreRequest(BaseModel):
-    url: str
+ #Entrada y salida de datos
+class VideoInput(BaseModel):
+    url: HttpUrl
 
-class GenreResponse(BaseModel):
+class GenrePrediction(BaseModel):
     genre: str
     confidence: float
 
-@app.post("/video/genre", response_model=GenreResponse)
-def classify_video(data: GenreRequest):
-    url = data.url.lower()
+class KidsPrediction(BaseModel):
+    safe: bool
 
-    #Simulación para probar web
-    if "music" in url:
-        return GenreResponse(genre="Música", confidence=0.92)
-    elif "game" in url:
-        return GenreResponse(genre="Videojuegos", confidence=0.88)
-    elif "edu" in url:
-        return GenreResponse(genre="Educación", confidence=0.90)
-    elif "sport" in url:
-        return GenreResponse(genre="Deportes", confidence=0.85)
-    else:
-        return GenreResponse(genre="Entretenimiento", confidence=0.75)
+
+# Extraccion de datos 
+def _get_data_and_predict(model, url: str):
+    """
+    Parametros entrada -> 
+    model: Modelo correspondiente a la predicción que queramos hacer. (Generos o kids).
+    url (string): Url del video que se quiere clasficar.
+
+    Parametros salida -> 
+        Predicción (string)
+
+    Extrae los datos de un video a partir de la url y predice el resultado
+    """
+    # Extracción de ID
+    video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11}).*", url)
+    if not video_id_match:
+        return None, 0.0
     
+    video_id = video_id_match.group(1)
+
+    # Extraemos los datos del video a partir del ID
+    df_video = get_info(video_id)
+    
+    if df_video is None or df_video.empty:
+        return None, 0.0
+
+    # Predicción
+    prediction= model.predict(df_video)[0]
+
+    try:
+        prob = model.predict_proba(df_video).max()
+    except:
+        prob = 0.95
+    
+    return prediction, prob
+
+
 @app.get("/video/genre", response_class=HTMLResponse)
 def genre_page(request: Request):
-    return templates.TemplateResponse(request, 
-                                      name = "genres.html", 
-                                      context = {"request": request})
+    return templates.TemplateResponse(request, name="genres.html")
+
+@app.get("/video", response_class=HTMLResponse)
+def video_page(request: Request):
+    return templates.TemplateResponse(request, name="video_check.html")
+
+# Predicciones
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(request, name='index.html')
+
+
+@app.post("/video/genre", response_model=GenrePrediction)
+async def predict_genre(video: VideoInput):
+    """Calsifica segun el género del video"""
+    label, prob = _get_data_and_predict(app.state.model_genre, str(video.url))
+
+    if label is None:
+        return GenrePrediction(genre="Error: URL no válida", confidence=0.0)
+
+    genre_name = app.state.encoder_genre.inverse_transform([label])[0]
+    
+    return GenrePrediction(genre=str(genre_name), confidence=float(prob))
+
+@app.post("/video/check", response_model=KidsPrediction)
+async def predict_kids(video: VideoInput):
+    """Clasifica si es apto para niños"""
+    label, prob = _get_data_and_predict(app.state.model_kids, str(video.url))
+
+    if label is None:
+        return KidsPrediction(safe=False) 
+    
+    is_safe = bool(label)
+    return KidsPrediction(safe=is_safe)
+
